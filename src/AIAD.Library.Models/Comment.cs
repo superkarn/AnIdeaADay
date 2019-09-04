043354832dcb904e68abfe11184b5e4804a86cc6
Karn Ratana <karn.ratana@gmail.com>
2019-09-04 02:50:34 +0000

    Add Comment.CreatorId

--- src/AIAD.Library.Models/Comment.cs
+++ src/AIAD.Library.Models/Comment.cs
@@ -4,10 +4,12 @@ namespace AIAD.Library.Models
 {
     public class Comment : BaseWithIdModel
     {
+        public string CreatorId { get; set; }
+        public ApplicationUser Creator { get; set; }
+
         public int IdeaId { get; set; }
         public Idea Idea { get; set; }
 
-        public bool IsEnabled { get; set; } = true;
         public string Content { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime LastModifiedDate { get; set; } = DateTime.UtcNow;
