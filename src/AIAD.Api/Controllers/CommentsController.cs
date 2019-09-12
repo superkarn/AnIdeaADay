599a59b3a0099c912b591c6c02dac892a94cc3b4
Karn Ratana <karn.ratana@gmail.com>
2019-09-12 00:29:36 +0000

    Add Comments to the API

---
+++ src/AIAD.Api/Controllers/CommentsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using AIAD.Library.Models;
+using AIAD.Library.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIAD.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class CommentsController : BaseController
+    {
+        private readonly ICommentService commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        // Get by commentId
+        [HttpGet]
+        [Route("api/comments/{id}")]
+        public ActionResult<Comment> Get(int id)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+
+            return this.commentService.GetById(this.Context, id);
+        }
+
+        // Get by ideaId
+        [HttpGet]
+        [Route("api/ideas/{ideaId}/comments")]
+        public ActionResult<IEnumerable<Comment>> GetByIdeaId(int ideaId)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+
+            return this.commentService.GetByIdeaId(this.Context, ideaId).ToList();
+        }
+
+        // Get by username
+        [HttpGet]
+        [Route("api/users/{username}/comments")]
+        public ActionResult<IEnumerable<Comment>> GetByUsername(string username)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+
+            return this.commentService.GetByCreatorUsername(this.Context, username).ToList();
+        }
+
+        // Create a comment to an idea
+        [HttpPost]
+        [Route("api/comments")]
+        public ActionResult Post(int ideaId, [FromBody] Comment value)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+
+            try
+            {
+                var newId = this.commentService.Create(this.Context, value);
+                return Created($"api/comments/{newId}", newId);
+            }
+            catch(Exception ex)
+            {
+                // TODO convert this into user-friendly response
+                // for now, return 409 Conflict
+                return Conflict(ex);
+            }
+        }
+
+        // TODO fix this.
+        //AmbiguousMatchException: The request matched multiple endpoints.Matches: 
+        //AIAD.Api.Controllers.CommentsController.Get(AIAD.Api)
+        //AIAD.Api.Controllers.CommentsController.Put(AIAD.Api)
+        //
+        // PUT api/users/{username}/ideas/5
+        [HttpPut]
+        [Route("api/comments/{id}")]
+        public ActionResult Put(int id, [FromBody] Comment value)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+            value.Id = id;
+
+            try
+            {
+                this.commentService.Update(this.Context, value);
+            }
+            catch (Exception ex)
+            {
+                // TODO convert this into user-friendly response
+                // for now, return 409 Conflict
+                return Conflict(ex);
+            }
+
+            return Ok();
+        }
+
+        // DELETE api/users/{username}/ideas/5
+        [HttpDelete]
+        [Route("api/comments/{id}")]
+        public ActionResult Delete(string username, int id)
+        {
+            this.Context = this.CreateApplicationContext((ClaimsIdentity)HttpContext.User.Identity);
+
+            try
+            {
+                this.commentService.DeleteById(this.Context, id);
+            }
+            catch (Exception ex)
+            {
+                // TODO convert this into user-friendly response
+                // for now, return 409 Conflict
+                return Conflict(ex);
+            }
+
+            return Ok();
+        }
+    }
+}
--- src/AIAD.Library.Services/CommentService.cs
+++ src/AIAD.Library.Services/CommentService.cs
@@ -1,53 +1,119 @@
-﻿using AIAD.Library.Models;
-using AIAD.Library.Data.Repositories.Interfaces;
+﻿using AIAD.Library.Data.Repositories.Interfaces;
+using AIAD.Library.Models;
 using AIAD.Library.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Security;
 
 namespace AIAD.Library.Services
 {
     public class CommentService : ICommentService
     {
-        private readonly IIdeaRepository commentRepository;
+        private readonly ICommentRepository commentRepository;
+        private readonly IApplicationUserRepository applicationUserRepository;
 
-        public CommentService(IIdeaRepository commentRepository)
+        public CommentService(ICommentRepository commentRepository, IApplicationUserRepository applicationUserRepository)
         {
             this.commentRepository = commentRepository;
+            this.applicationUserRepository = applicationUserRepository;
         }
 
         public int Create(ApplicationContext context, Comment item)
         {
-            throw new NotImplementedException();
+            return this.commentRepository.Create(item);
         }
 
         public void DeleteById(ApplicationContext context, int id)
         {
-            throw new NotImplementedException();
+            // If there's no current user, don't allow
+            if (context.CurrentUser == null)
+            {
+                throw new SecurityException($"User NULL is unauthorized to delete Idea id {id}");
+            }
+
+            var item = this.commentRepository.GetById(id);
+
+            // If the item doesn't exist, treat it as if the item has been deleted
+            if (item == null)
+            {
+                return;
+            }
+
+            // TODO allow Admin 
+            // If the current user is not the item creator, don't allow
+            if (item.CreatorId != context.CurrentUser.Id)
+            {
+                throw new SecurityException($"User {context.CurrentUser.Id} is unauthorized to delete Idea id {id}");
+            }
+
+            try
+            {
+                this.commentRepository.DeleteById(id);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // This exception is expected when trying to delete an item that does not exist.
+                // We catch it and let it through, because the end result is as intially requested: the item no longer exists.
+                // For any other exception, we let it bubble up.
+                if (!ex.Message.Contains("Database operation expected to affect 1 row(s) but actually affected 0 row(s)"))
+                {
+                    throw ex;
+                }
+            }
         }
 
         public IEnumerable<Comment> GetByCreatorId(ApplicationContext context, string creatorId)
         {
-            throw new NotImplementedException();
+            return this.commentRepository.GetByCreatorId(creatorId);
         }
 
         public IEnumerable<Comment> GetByCreatorUsername(ApplicationContext context, string username)
         {
-            throw new NotImplementedException();
+            var user = this.applicationUserRepository.GetByUsername(username).FirstOrDefault();
+
+            return this.commentRepository.GetByCreatorId(user.Id);
         }
 
         public Comment GetById(ApplicationContext context, int id)
         {
-            throw new NotImplementedException();
+            return this.commentRepository.GetById(id);
         }
 
         public IEnumerable<Comment> GetByIdeaId(ApplicationContext context, int ideaId)
         {
-            throw new NotImplementedException();
+            return this.commentRepository.GetByIdeaId(ideaId);
         }
 
         public void Update(ApplicationContext context, Comment item)
         {
-            throw new NotImplementedException();
+            // If there's no current user, don't allow
+            if (context.CurrentUser == null)
+            {
+                throw new SecurityException($"User NULL is unauthorized to delete Comment id {item.Id}");
+            }
+
+            var existingItem = this.commentRepository.GetById(item.Id);
+
+            // If the item doesn't exist, throw an exception
+            if (existingItem == null)
+            {
+                throw new NullReferenceException($"Comment id {item.Id} does not exist.");
+            }
+
+            // TODO allow Admin 
+            // If the current user is not the item creator, don't allow
+            if (existingItem.CreatorId != context.CurrentUser.Id)
+            {
+                throw new SecurityException($"User {context.CurrentUser.Id} is unauthorized to update Comment id {item.Id}.");
+            }
+
+            // TODO make this work with PATCH
+            existingItem.Content = item.Content;
+            existingItem.LastModifiedDate = DateTime.UtcNow;
+
+            this.commentRepository.Update(existingItem);
         }
     }
 }
