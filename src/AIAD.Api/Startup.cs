b6d54817d01eb27f1da5a4de4d81a08f590e3447
Karn Ratana <karn.ratana@gmail.com>
2019-11-19 22:55:44 -0500

    Update AIAD.Api to .net core 3.0 (by creating it from scratch, and copying the files over manually)

--- src/AIAD.Api/Startup.cs
+++ src/AIAD.Api/Startup.cs
@@ -1,14 +1,20 @@
-﻿using AIAD.Library.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIAD.Library.Global;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Swashbuckle.AspNetCore.Swagger;
-using System;
-using System.Text;
+using Microsoft.OpenApi.Models;
 
 namespace AIAD.Api
 {
@@ -29,19 +35,16 @@ namespace AIAD.Api
             Application.Authentication.BaseUrl = this.Configuration.GetSection("Applications")["Authentication:BaseUrl"];
             Application.Web.BaseUrl = this.Configuration.GetSection("Applications")["Web:BaseUrl"];
 
-            services.AddRazorPages();
+            services.AddControllers();
 
             services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
             #region JWT
-            var jwtAppSettings = new JwtAppSettings();
-            var jwtAppSettingsSection = this.Configuration.GetSection("Jwt");
-            jwtAppSettingsSection.Bind(jwtAppSettings);
-
-            services.Configure<JwtAppSettings>(jwtAppSettingsSection);
+            var jwtAppSettings = this.Configuration.GetSection("Jwt").Get<JwtAppSettings>();
+            services.AddSingleton<JwtAppSettings>(jwtAppSettings);
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -67,7 +70,7 @@ namespace AIAD.Api
                         .AllowAnyOrigin() // TODO replace this with WithOrigins()
                         .AllowAnyMethod()
                         .AllowAnyHeader()
-                        .AllowCredentials()
+                        //.AllowCredentials() // TODO uncomment this when authentication is ready
                         .Build());
             });
             #endregion
@@ -89,30 +92,27 @@ namespace AIAD.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             // https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-2.2#enabling-cors-with-middleware
             // CORS Middleware must precede any defined endpoints in your app where you want to support cross-origin requests 
             // (for example, before the call to UseMvc for MVC/Razor Pages Middleware).
             app.UseCors("CorsPolicy");
 
-            app.UseAuthentication();
-            app.UseHttpsRedirection();
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
             });
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
-            app.UseAuthentication();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+                endpoints.MapControllers();
             });
         }
     }
